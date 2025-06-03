46e002af19ba56ebb3014c5a11bfed175a98fdff
Patrick Kernan <pkernan18@yahoo.com>
2025-06-03 13:59:11 -0400

    Adding profile button to movie-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { ProfilePageComponent } from './profile-page/profile-page.component';
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
+  { path: 'profile', component: ProfilePageComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
 ];
 
--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { UserRegistrationService } from '../fetch-api-data.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
   movies: any[] = [];
-  constructor(public fetchApiData: UserRegistrationService ) { }
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.getMovies();
@@ -20,5 +24,9 @@ export class MovieCardComponent {
       console.log(this.movies);
       return this.movies;
     });
+  }
+
+  goToProfile(): void {
+    this.router.navigate(['profile']);
   }
 }
