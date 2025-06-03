5751b6478a39c882c9d1899f813330cb9cc59990
Patrick Kernan <pkernan18@yahoo.com>
2025-06-03 12:19:07 -0400

    6.4 update

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { UserRegistrationService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login-form',
@@ -9,28 +10,29 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./user-login-form.component.scss']
 })
 export class UserLoginFormComponent implements OnInit {
-@Input() userData = { Username: '', Password: '' };
+  @Input() userData = { Username: '', Password: '' };
 
-constructor(
+  constructor(
     public fetchApiData: UserRegistrationService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar) { }
+    public snackBar: MatSnackBar,
+    public router: Router) { }
 
-ngOnInit(): void {
-}
+  ngOnInit(): void { }
 
-// This is the function responsible for sending the form inputs to the backend
-loginUser(): void {
-  this.fetchApiData.userLogin(this.userData).subscribe(
-    (result) => {
-      this.dialogRef.close();
-      this.snackBar.open('Login successful!', 'OK', { duration: 2000 });
-    },
-    (error) => {
-      const errMsg = error?.error?.message || error?.error || 'Login failed. Please try again.';
-      this.snackBar.open(errMsg, 'OK', { duration: 2000 });
-    }
-  );
-}
-
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe(
+      (result) => {
+        localStorage.setItem('token', result.token);
+        localStorage.setItem('user', result.user.Username);
+        this.dialogRef.close();
+        this.snackBar.open('Login successful!', 'OK', { duration: 2000 });
+        this.router.navigate(['movies']);
+      },
+      (error) => {
+        const errMsg = error?.error?.message || error?.error || 'Login failed. Please try again.';
+        this.snackBar.open(errMsg, 'OK', { duration: 2000 });
+      }
+    );
+  }
 }
--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,14 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-
-// You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
-
-// This import brings in the API calls we created in 6.2
 import { UserRegistrationService } from '../fetch-api-data.service';
-
-// This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
-
 
 @Component({
   selector: 'app-user-registration-form',
@@ -16,22 +9,18 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./user-registration-form.component.scss']
 })
 export class UserRegistrationFormComponent implements OnInit {
-
   @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
 
-constructor(
+  constructor(
     public fetchApiData: UserRegistrationService,
     public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
     public snackBar: MatSnackBar) { }
 
-ngOnInit(): void {
-}
+  ngOnInit(): void { }
 
-// This is the function responsible for sending the form inputs to the backend
-registerUser(): void {
+  registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
-  // Logic for a successful user registration goes here! (To be implemented)
-     this.dialogRef.close(); // This will close the modal on success!
+     this.dialogRef.close();
      console.log(result);
      this.snackBar.open(result, 'OK', {
         duration: 2000
@@ -43,5 +32,4 @@ registerUser(): void {
       });
     });
   }
-
-  }
+}
